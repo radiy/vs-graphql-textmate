55993889f4a1524138fcaadfec92914b7eac963e
Roman Kvasov <kvasov.roman@gmail.com>
2020-08-15 17:33:21 +0300

    simple colorization

---
+++ GraphQL/GraphQLClassifierProvider.cs
@@ -0,0 +1,113 @@
+﻿using Microsoft.VisualStudio.Text.Tagging;
+using Microsoft.VisualStudio.Text;
+using System.ComponentModel.Composition;
+using Microsoft.VisualStudio.Utilities;
+using Microsoft.VisualStudio.Text.Classification;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Media;
+
+namespace GraphQL
+{
+    [Export(typeof(ITaggerProvider))]
+    [ContentType("graphql")]
+    [TagType(typeof(ClassificationTag))]
+    internal sealed class GraphQLClassifierProvider : ITaggerProvider
+    {
+
+        [Export]
+        [Name("graphql")]
+        [BaseDefinition("code")]
+        internal static ContentTypeDefinition GraphQLContentType = null;
+
+        [Export]
+        [FileExtension(".graphql")]
+        [ContentType("graphql")]
+        internal static FileExtensionToContentTypeDefinition GraphQLFileType = null;
+
+        [Import]
+        internal IClassificationTypeRegistryService ClassificationTypeRegistry = null;
+
+        [Import]
+        internal IBufferTagAggregatorFactoryService aggregatorFactory = null;
+
+        public ITagger<T> CreateTagger<T>(ITextBuffer buffer) where T : ITag
+        {
+            return (ITagger<T>)new Tagger(ClassificationTypeRegistry);
+        }
+    }
+
+    [Export(typeof(EditorFormatDefinition))]
+    [ClassificationType(ClassificationTypeNames = "mutation")]
+    [Name("mutation")]
+    [UserVisible(false)]
+    [Order(Before = Priority.Default)]
+    internal sealed class MutationDefinition : ClassificationFormatDefinition
+    {
+        public MutationDefinition()
+        {
+            DisplayName = "mutation"; 
+            ForegroundColor = Colors.BlueViolet;
+        }
+    }
+
+    [Export(typeof(EditorFormatDefinition))]
+    [ClassificationType(ClassificationTypeNames = "query")]
+    [Name("query")]
+    [UserVisible(false)]
+    [Order(Before = Priority.Default)]
+    internal sealed class QueryDefinition : ClassificationFormatDefinition
+    {
+        public QueryDefinition()
+        {
+            DisplayName = "query";
+            ForegroundColor = Colors.BlueViolet;
+        }
+    }
+
+    internal static class OrdinaryClassificationDefinition
+    {
+        [Export(typeof(ClassificationTypeDefinition))]
+        [Name("query")]
+        internal static ClassificationTypeDefinition query = null;
+
+        [Export(typeof(ClassificationTypeDefinition))]
+        [Name("mutation")]
+        internal static ClassificationTypeDefinition mutation = null;
+    }
+
+    class Tagger : ITagger<ClassificationTag>
+    {
+        private Dictionary<string, IClassificationType> types = new Dictionary<string, IClassificationType>();
+
+        public Tagger(IClassificationTypeRegistryService typeService)
+        {
+            types["mutation"] = typeService.GetClassificationType("mutation");
+            types["query"] = typeService.GetClassificationType("query");
+        }
+
+        public event EventHandler<SnapshotSpanEventArgs> TagsChanged;
+
+        public IEnumerable<ITagSpan<ClassificationTag>> GetTags(NormalizedSnapshotSpanCollection spans)
+        {
+            foreach (SnapshotSpan curSpan in spans)
+            {
+                ITextSnapshotLine containingLine = curSpan.Start.GetContainingLine();
+                int curLoc = containingLine.Start.Position;
+                string[] tokens = containingLine.GetText().ToLower().Split(' ');
+
+                foreach (string token in tokens)
+                {
+                    if (types.TryGetValue(token, out var value))
+                    {
+                        var tokenSpan = new SnapshotSpan(curSpan.Snapshot, new Span(curLoc, token.Length));
+                        if (tokenSpan.IntersectsWith(curSpan))
+                            yield return new TagSpan<ClassificationTag>(tokenSpan, new ClassificationTag(value));
+                    }
+                    curLoc += token.Length + 1;
+                }
+            }
+        }
+    }
+}
---
+++ GraphQL/GraphQLPackage.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+using Microsoft.VisualStudio.Shell;
+using Task = System.Threading.Tasks.Task;
+using Microsoft.VisualStudio.Text.Tagging;
+using Microsoft.VisualStudio.Text;
+using System.ComponentModel.Composition;
+using Microsoft.VisualStudio.Utilities;
+
+namespace GraphQL
+{
+    /// <summary>
+    /// This is the class that implements the package exposed by this assembly.
+    /// </summary>
+    /// <remarks>
+    /// <para>
+    /// The minimum requirement for a class to be considered a valid package for Visual Studio
+    /// is to implement the IVsPackage interface and register itself with the shell.
+    /// This package uses the helper classes defined inside the Managed Package Framework (MPF)
+    /// to do it: it derives from the Package class that provides the implementation of the
+    /// IVsPackage interface and uses the registration attributes defined in the framework to
+    /// register itself and its components with the shell. These attributes tell the pkgdef creation
+    /// utility what data to put into .pkgdef file.
+    /// </para>
+    /// <para>
+    /// To get loaded into VS, the package must be referred by &lt;Asset Type="Microsoft.VisualStudio.VsPackage" ...&gt; in .vsixmanifest file.
+    /// </para>
+    /// </remarks>
+    [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
+    [Guid(GraphQLPackage.PackageGuidString)]
+    public sealed class GraphQLPackage : AsyncPackage
+    {
+        /// <summary>
+        /// GraphQLPackage GUID string.
+        /// </summary>
+        public const string PackageGuidString = "6bae39ce-4d59-42cc-b941-f0d18b585b0c";
+
+        #region Package Members
+
+        /// <summary>
+        /// Initialization of the package; this method is called right after the package is sited, so this is the place
+        /// where you can put all the initialization code that rely on services provided by VisualStudio.
+        /// </summary>
+        /// <param name="cancellationToken">A cancellation token to monitor for initialization cancellation, which can occur when VS is shutting down.</param>
+        /// <param name="progress">A provider for progress updates.</param>
+        /// <returns>A task representing the async work of package initialization, or an already completed task if there is none. Do not return null from this method.</returns>
+        protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
+        {
+            // When initialized asynchronously, the current thread may be a background thread at this point.
+            // Do any initialization that requires the UI thread after switching to the UI thread.
+            await this.JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
+        }
+
+        #endregion
+    }
+
+    [Export(typeof(ITaggerProvider))]
+    [ContentType("graphql")]
+    [TagType(typeof(GraphQLTag))]
+    internal sealed class GraphQLTokenTagProvider : ITaggerProvider
+    {
+
+        public ITagger<T> CreateTagger<T>(ITextBuffer buffer) where T : ITag
+        {
+            return null;
+            //return new OokTokenTagger(buffer) as ITagger<T>;
+        }
+    }
+
+    public class GraphQLTag : ITag
+    {
+        public GraphQLTag()
+        {
+        }
+    }
+}
